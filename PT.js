c7bd124a67f2e609879c0ce7a16acce86de3a5a2
Adam Łukawski <63563505+sunsetsobserver@users.noreply.github.com>
2023-11-10 02:20:05 +0100

    Add multidimensionality PT.js

--- PT.js
+++ PT.js
@@ -1,41 +1,50 @@
 class PerformativeTransaction {
     constructor(func, referencedTransactions = []) {
-        if (referencedTransactions.length) {
-            this.func = (n, xArray) => {
-                const positions = func(n);
-                return positions.map((position, index) => {
-                    return referencedTransactions[index].func(position, xArray[index]);
-                });
-            };
-        } else {
-            // Otherwise, use the provided function directly
-            this.func = func;
-        }
+        this.func = func;
+        this.referencedTransactions = referencedTransactions;
     }
 
     getValues(steps, xArray) {
         let values = [];
         for (let i = 0; i < steps; i++) {
-            values.push(this.func(i, xArray));
+            let result;
+            if (this.referencedTransactions.length > 0) {
+                // Apply the function to get positions for each dimension
+                const positions = this.func(i, ...xArray);
+                // For each dimension, use the position to get the value from the corresponding referenced transaction
+                result = positions.map((position, index) => {
+                    if (index < this.referencedTransactions.length) {
+                        return this.referencedTransactions[index].getValues(1, [position])[0];
+                    }
+                    return position;
+                });
+            } else {
+                // If there are no referenced transactions, apply the function directly
+                result = this.func(i, ...xArray);
+            }
+            values.push(result);
         }
         return values;
     }
 }
 
-// Define transactions without needing x1 and x2
+// Define transactions without needing specific x1 and x2
 const chromaticTransaction = new PerformativeTransaction((n, x) => x * Math.pow(2, n/12));
 const velocityTransaction = new PerformativeTransaction((n, x) => x + 0.05 * n);
+const otherTransaction = new PerformativeTransaction((n, x) => x + 3 * n);
 
-const multiDimPattern = (n) => {
-    return [n * 2, n + 0.5];
+// Define a multi-dimensional pattern function
+const multiDimPattern = (n, ...args) => {
+    return args.map((x, index) => x + n * (index + 1));
 };
 
-const multiDimTransaction = new PerformativeTransaction(multiDimPattern, [chromaticTransaction, velocityTransaction]);
+const testTransaction = new PerformativeTransaction((n, x) => x * Math.pow(2, n/12));
+//console.log(testTransaction.getValues(12, [131]));
 
-// Sampling:
-/* const x1 = 261.6256;  // Starting pitch
-const x2 = 0.5;      // Starting velocity
-console.log(multiDimTransaction.getValues(20, [x1, x2])); */
+const thisFunction = (n, x) => {x + n};
+
+const newTransaction = new PerformativeTransaction(thisFunction, [chromaticTransaction]);
+console.log(newTransaction.getValues(12, [313]))
 
 const majorPattern = (n) => {
     const scaleSteps = [0, 2, 4, 5, 7, 9, 11]; // Major scale intervals relative to chromatic scale
@@ -47,9 +56,22 @@ const majorPattern = (n) => {
 
 const majorScaleTransaction = new PerformativeTransaction(majorPattern, [chromaticTransaction]);
 
+function majorScale(rootNote) {
+    // Major scale pattern in terms of semitones: R, W, W, H, W, W, W, H
+    const pattern = [0, 2, 4, 5, 7, 9, 11, 12];
+    return pattern.map(interval => chromaticTransaction.getValues(1, [rootNote + interval])[0]);
+}
+
 // Sampling:
-const x1 = 261.6256;  // Starting pitch (C4 frequency for example)
-console.log(majorScaleTransaction.getValues(14, [x1]));
+/* const x1 = 261.6256;  // Starting pitch (C4 frequency for example)
+console.log(majorScaleTransaction.getValues(14, [x1])); */
+
+// Create a Performative Transaction with multiple dimensions
+const multiDimTransaction = new PerformativeTransaction(multiDimPattern, [chromaticTransaction, velocityTransaction, majorScaleTransaction]);
+
+// Sampling:
+const xArray = [261.6256, 0.5, 440];  // Starting pitch and velocity as an array
+//console.log(multiDimTransaction.getValues(5, xArray));
 
 
 
